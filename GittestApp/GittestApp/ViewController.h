0025221e9e31d2a1ea695cd0c28d90070499d373
Subair Ahamed <subair301@gmail.com>
2018-09-17 14:34:44 +0300

    Upload folder

---
+++ GittestApp/GittestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GittestApp
+//
+//  Created by Subair Ahmed on 9/17/18.
+//  Copyright © 2018 Subair Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GittestApp/GittestApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GittestApp
+//
+//  Created by Subair Ahmed on 9/17/18.
+//  Copyright © 2018 Subair Ahmed. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
